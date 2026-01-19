9f32137525425a4fa1ee4fcd93efdea4123539dc
Joseph-Kimuhu <joseph.kimuhu@student.moringaschool.com>
2026-01-19 17:12:45 +0300

    final final commit

--- server/app.py
+++ server/app.py
@@ -3,7 +3,7 @@
 from flask import Flask, make_response, jsonify, session
 from flask_migrate import Migrate
 
-from models import db, Article, User
+from .models import db, Article, User
 
 app = Flask(__name__)
 app.secret_key = b'Y\xf1Xz\x00\xad|eQ\x80t \xca\x1a\x10K'
--- server/testing/conftest.py
+++ server/testing/conftest.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+import pytest
+from ..app import app, db
+from ..models import Article, User
+from faker import Faker
+from random import randint
+
 def pytest_itemcollected(item):
     par = item.parent.obj
     node = item.obj
@@ -7,3 +13,28 @@ def pytest_itemcollected(item):
     suf = node.__doc__.strip() if node.__doc__ else node.__name__
     if pref or suf:
         item._nodeid = ' '.join((pref, suf))
+
+@pytest.fixture(scope='session', autouse=True)
+def setup_database():
+    with app.app_context():
+        db.create_all()
+        fake = Faker()
+        print("Creating users...")
+        users = [User(name=fake.name()) for i in range(25)]
+        db.session.add_all(users)
+        print("Creating articles...")
+        articles = []
+        for i in range(100):
+            content = fake.paragraph(nb_sentences=8)
+            preview = content[:25] + '...'
+            article = Article(
+                author=fake.name(),
+                title=fake.sentence(),
+                content=content,
+                preview=preview,
+                minutes_to_read=randint(1,20),
+            )
+            articles.append(article)
+        db.session.add_all(articles)
+        db.session.commit()
+        print("Database setup complete.")
